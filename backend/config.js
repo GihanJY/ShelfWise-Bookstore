f0c719eca5fbb03291f472b95360f810f9e09f00
GihanJY <123gihanjaya123@gmail.com>
2024-11-20 23:37:00 +0530

    Ignore backend/config.js

--- backend/config.js
+++
@@ -1,3 +0,0 @@
-export const PORT = 5555;
-
-export const mongoDBURL = 'mongodb+srv://gihandbconnection:DRtb4Dp1mPhx57d3@gihanscluster.ki3xk.mongodb.net/?retryWrites=true&w=majority&appName=GihansCluster';
